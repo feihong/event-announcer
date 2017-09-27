0bd7f7b8341bb6d8e680307b3495382f8437b5a2
Feihong Hsu <feihong.hsu@gmail.com>
2017-09-27 18:53:52 -0500

    Download all film pages

--- scripts/chicago_film_festival.exs
+++ scripts/chicago_film_festival.exs
@@ -7,8 +7,18 @@ defmodule Main do
     film_urls =
       Floki.find(html, "ul.film-list li a")
       |> Floki.attribute("href")
+      |> Enum.filter(fn url -> not String.contains?(url, "/special-architecture/") end)
+
+    film_urls
+    |> Enum.map(&download_film_page/1)
 
     IO.puts "Found #{length(film_urls)} films"
+  end
+
+  def download_film_page(url) do
+    slug = String.split(url, "/", trim: true)
+    |> List.last()
+    Download.fetch_page("chifilmfest__#{slug}", url, %{})
   end
 end
 
