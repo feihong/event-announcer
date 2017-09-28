af55097a86631b1167e9ab993572ba397451cf53
Feihong Hsu <feihong.hsu@gmail.com>
2017-09-27 19:15:06 -0500

    Extract metadata in human-readable way

--- scripts/chicago_film_festival.exs
+++ scripts/chicago_film_festival.exs
@@ -9,16 +9,40 @@ defmodule Main do
       |> Floki.attribute("href")
       |> Enum.filter(fn url -> not String.contains?(url, "/special-architecture/") end)
 
-    film_urls
-    |> Enum.map(&download_film_page/1)
+    events =
+      film_urls
+      |> Enum.map(&download_film_page/1)
+      |> Enum.take(1)
+      |> Enum.map(&convert/1)
 
     IO.puts "Found #{length(film_urls)} films"
+
+    IO.puts List.first(events)[:description]
   end
 
   def download_film_page(url) do
     slug = String.split(url, "/", trim: true)
     |> List.last()
     Download.fetch_page("chifilmfest__#{slug}", url, %{})
+  end
+
+  def convert(html) do
+    main = Floki.find(html, "section#main")
+    title = main |> Floki.find("h1") |> Floki.text
+    meta =
+      Floki.find(main, ".film-credits li")
+      |> Enum.map(fn li ->
+          label = Floki.find(li, "label") |> Floki.text
+          value = Floki.filter_out(li, "label") |> Floki.text |> String.trim
+          "#{label}: #{value}"
+         end)
+      |> Enum.join("\n")
+
+    synopsis = Floki.find(main, ".film-synopsis") |> Floki.text
+    %{
+      name: title,
+      description: meta,
+    }
   end
 end
 
