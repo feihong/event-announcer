b5cdd9901fc37ed07562a55f90fba33b712852d3
Feihong Hsu <feihong.hsu@gmail.com>
2017-09-28 22:06:32 -0500

    Do keyword matching for adhoc events

--- lib/events/adhoc.ex
+++ lib/events/adhoc.ex
@@ -1,12 +1,15 @@
 defmodule Events.Adhoc do
   @events_file "adhoc_events.json"
+  @keywords Application.fetch_env!(:events, :keywords)
 
   @doc """
   Read adhoc events from a JSON file.
   """
   def fetch_all() do
     if File.exists?(@events_file) do
-      Events.Util.from_json_file(@events_file)
+      @events_file
+      |> Events.Util.from_json_file()
+      |> Enum.map(fn evt -> Events.Util.match_keywords(evt, @keywords) end)
     else
       []
     end
